9e1df51dabd2bf8eefe507a04c41460b5af2a3cb
usermine12 <mikaeljarv@gmail.com>
2020-04-28 14:40:43 +0300

    comments now show when it was sent, changed favicon and manifest

--- src/components/ProductComment.js
+++ src/components/ProductComment.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {
-  Typography
+  Typography,
+  Box
 } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 
@@ -18,9 +19,35 @@ const useStyles = makeStyles(() => ({
 const ProductComment = ({ comment }) => {
   const classes = useStyles()
 
+  const commentDate = Date.parse(comment.date)
+  const currentDate = new Date()
+  let timeDiff = (currentDate - commentDate) / (1000 ) //diff in seconds
+  let timeUnit = 'seconds'
+  if (timeDiff >= 60) {
+    timeDiff = timeDiff / 60  //diff in minutes
+    timeUnit = 'minutes'
+  }
+  if (timeDiff >= 60) {
+    timeDiff = timeDiff / 60 //diff in hours
+    timeUnit = 'hours'
+  }
+  if (timeDiff >= 24) {
+    timeDiff = timeDiff / 24 //diff in days
+    timeUnit = 'days'
+  }
+
+  timeDiff = parseInt(timeDiff, 10)
+
   return (
     <div className={classes.commentDiv}>
-      <Typography>{comment.author}</Typography>
+      <Box display='flex'>
+        <Typography variant='subtitle1'>{comment.author}</Typography> 
+        <Box flexGrow={1} display='flex' justifyContent='flex-end'>
+          <Typography variant='caption'>
+            {timeDiff} {timeUnit} ago
+          </Typography>
+        </Box>
+      </Box>
       <Typography variant='caption'>{comment.message}</Typography>
     </div>
   )
